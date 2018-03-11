7fea4e77020200a46b2b7e89efd347317543f776
13dev <qwerty124563@gmail.com>
2018-03-11 19:07:16 +0000

    add doLogin method

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,70 @@
 <template>
 	<div class="wrapper">
-		<form class="form-signin">       
+		<form class="form-signin">
 			<h2 class="form-signin-heading">Please login</h2>
-			<input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
-			<input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+			<input
+			v-model="email"
+			type="text"
+			class="form-control"
+			name="username"
+			placeholder="Email Address"
+			required=""
+			autofocus="" />
+
+			<input
+			v-model="password"
+			type="password"
+			class="form-control"
+			name="password"
+			placeholder="Password"
+			required=""/>
 			<label class="checkbox">
 				<input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
 			</label>
-			<button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+			<button class="btn btn-lg btn-primary btn-block" @click.prevent="doLogin">Login</button>   
 		</form>
 	</div>
 </template>
 <script>
-	
+	export default {
+		name: 'Login',
+		data() {
+			return {
+				email: 'lesch.chesley@feil.biz',
+				password: 'secret'
+			}
+		},
+		methods: {
+			doLogin() {
+				this.$log.debug('doLogin()')
+				if(this.$auth.isAuthenticated()){
+					this.$router.push({
+						name:'Home'
+					})
+				}
+				this.axios.post('auth/login',{
+					email: this.email,
+					password: this.password
+				})
+				.catch(response => {
+					// Catch error
+					this.$log.debug('Catch - doLogin()',response.data.code, response.data.message)
+				})
+				.then(response => {
+					this.$auth.setToken(response.data.data.token, response.data.data.expires_in + Date.now())
+					this.$log.debug('Then - doLogin()', response.data.code, response.data.message)
+
+					// Redirect
+					this.$router.push({
+						name:'Home'
+					})
+				})
+			}
+		}
+	}
 </script>
 <style lang="scss" scoped>
-.wrapper {	
+.wrapper {
 	margin-top: 80px;
  	margin-bottom: 80px;
 }
@@ -25,7 +74,7 @@
   padding: 15px 35px 45px;
   margin: 0 auto;
   background-color: #fff;
-  border: 1px solid rgba(0,0,0,0.1);  
+  border: 1px solid rgba(0,0,0,0.1);
 
   .form-signin-heading,
 	.checkbox {
