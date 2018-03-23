fac7f9dbda4f9ba45d1f1aaf4cd34b53cc348b6e
Aluno <Aluno@ebsaasilva.edu>
2018-03-23 11:03:05 +0000

    add style profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -12,28 +12,58 @@
     		</div>
 			<div class="profile-nav">
                 <div class="row">
-                    <div class="col-sm-12 col-md-4">
+                    <div class="col-md-4">
                         <img src="../assets/publication.png" class="img-fluid">
                         <h4>5 PUBLICAÇÕES</h4>
                     </div>
-                    <div class="col-sm-12 col-md-4">
+                    <div class="col-md-4">
                         <img src="../assets/like.png" class="img-fluid">
                         <h4>10 GOSTOS</h4>
                     </div>
-                    <div class="col-sm-12 col-md-4">
+                    <div class="col-md-4">
                         <img src="../assets/followers.png" class="img-fluid">
                         <h4>11 SEGUIDORES</h4>
                     </div>
                 </div>
             </div>
     		</div>
-		<ul>
-			<li>ID: 			{{ user.id }}</li>
-			<li>birthday: 		{{ user.birthday }}</li>
-			<li>created_at: 	{{ user.created_at }}</li>
-			<li>email: 			{{ user.email }}</li>
-			<li>gender:  		{{ user.gender }}</li>
-		</ul>
+		<div class="row w-100">
+			<div class="col-md-4" style="padding-right: 0;">
+				<div class="profile-about profile-content-box profile-sidebar">
+					<ul>
+						<li>ID: 			{{ user.id }}</li>
+						<li>birthday: 		{{ user.birthday }}</li>
+						<li>created_at: 	{{ user.created_at }}</li>
+						<li>email: 			{{ user.email }}</li>
+						<li>gender:  		{{ user.gender }}</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-8" style="padding-left: 0;padding-right: 0;">
+				<div class="profile-question profile-content-box profile-sidebar">
+					<div class="media">
+						<a href="#">
+						  <img class="align-self-start mr-3" width="50" :src="user.avatar" alt="Image">
+						</a>
+					  <div class="media-body">
+					  	<div>
+					  		<h5 class="mt-0">Top-aligned media</h5>
+					  		<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. </p>	
+					  	</div>
+
+				    	<div style="margin-left:-70px;">
+					  	<hr style="margin: 10px 0 5px 0; border-top: 1px solid rgba(60, 60, 60, 0.12);"/>
+	                    <a href="#">
+	                        <i class="fa fa-heart-o fa-2x" style="color: #DA4453; font-size: 1.3em;margin:6px;" aria-hidden="true"></i>
+	                        <label style="color: #DA4453;position: relative;top: -4px; font-weight:700;">10
+	                        </label>
+	                    </a>
+	                    </div>
+					  </div>	
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -129,12 +159,14 @@
     /* max-width: 30%; */
     width: 100px;
     height: 100px;
+    object-fit: cover;
 
 }
 
 .cover > img {
     width: 100%;
     max-height: 250px;
+    object-fit: cover;
 }
 
 .profile-name {
@@ -142,6 +174,10 @@
     margin-top: -40px;
     margin-left: 150px;
 
+}
+
+.profile-sidebar {
+    margin: 10px 0 10px 10px;
 }
 
 .profile-name > h4 {
@@ -175,4 +211,35 @@
   font-size: 20px;
   margin-top: 10px;
 }
+
+.profile-content-box {
+    border: 1px solid #eaeaea;
+    border-radius: 0px;
+    padding: 10px;
+}
+
+.profile-question:not(:last-child) {
+    border-bottom: 2px solid #fff;
+}
+
+.profile-question {
+    padding: 10px 10px 0 10px;
+    background-color: #ffffff;
+    border: 1px solid #e7e7e7;
+}
+
+.profile-question img {
+    width: 60px;
+	height: 60px;
+   /* border-radius: 50%; */
+}
+
+.profile-question h5 {
+    display: inline-block;
+}
+
+/*.profile-question p:first-of-type {
+    display: inline-block;
+    float: right;
+} */
 </style>
